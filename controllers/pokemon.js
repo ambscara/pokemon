997a586ddf753d4bca16e33d6fc542900b368fa4
ambscara <150319615+ambscara@users.noreply.github.com>
2024-03-24 11:06:33 -0700

    added findById route

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -4,16 +4,23 @@ const Pokemon = require('../models/Pokemon')
 router.get('/', async (req, res) => {
     try {
         const pokemon = await Pokemon.find()
-            
-            
-     res.json(pokemon)
+        res.json(pokemon)
     }catch (error) {
         console.log('error fetching all pokemon', error)
         res.status(500).json({message: 'error fetching all pokemon'})
     }
 })
 
-
+router.get('/:pokemonId', async (req, res) => {
+    try {
+        const { pokemonId } = req.params
+        const pokemon = await Pokemon.find({ pokemonId })
+        res.json(pokemon)
+    } catch (error) {
+        console.log('Error fetching pokemon by id', error)
+        res.status(500).json({ message: 'error fetching pokemon by id' })
+    }
+})
 
 module.exports = router
 
